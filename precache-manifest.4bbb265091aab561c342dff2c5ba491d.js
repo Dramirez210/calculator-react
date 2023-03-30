b0687ef0217f27e75f79f153e4636ac081454cfa
Dramirez <funkadelick485@gmail>
2023-03-29 21:29:20 -0600

    Updates

---
+++ precache-manifest.4bbb265091aab561c342dff2c5ba491d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a72bf417053a6d5bdbe",
+    "url": "/calculator-react/static/css/main.403f81aa.chunk.css"
+  },
+  {
+    "revision": "3a72bf417053a6d5bdbe",
+    "url": "/calculator-react/static/js/main.3a72bf41.chunk.js"
+  },
+  {
+    "revision": "ad523103a93efff5221c",
+    "url": "/calculator-react/static/js/1.ad523103.chunk.js"
+  },
+  {
+    "revision": "180f923038051d0b454f",
+    "url": "/calculator-react/static/js/runtime~main.180f9230.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/calculator-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ae768545ee7c351034dfe9acadad8bd3",
+    "url": "/calculator-react/static/media/Logouacm.ae768545.png"
+  },
+  {
+    "revision": "3da218842ace7d075122351d1e400692",
+    "url": "/calculator-react/index.html"
+  }
+];
